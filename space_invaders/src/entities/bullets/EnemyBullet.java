0e1ec4573da8e19681a31f697a9a72a66936e76d
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-06-08 22:06:24 -0300

    removed files from repositories

--- space_invaders/src/entities/bullets/EnemyBullet.java
+++
@@ -1,9 +0,0 @@
-package entities.bullets;
-
-public class EnemyBullet extends Bullet{
-
-	public EnemyBullet(int x, int y, int width, int height, double speed) {
-		super(x, y, width, height, speed);
-	}
-
-}
